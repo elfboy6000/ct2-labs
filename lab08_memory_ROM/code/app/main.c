6217c5f57bd54a9e63d9825b008dfa044d6f4035
islamarb <islamarb@students.zhaw.ch>
2026-04-14 18:34:42 +0200

    Finished praktika 8

--- lab08_memory_ROM/code/app/main.c
+++ lab08_memory_ROM/code/app/main.c
@@ -19,17 +19,32 @@
 
 /// STUDENTS: To be programmed
 
+#include <reg_stm32f4xx.h>
+#include <reg_ctboard.h>
+#include "hal_ct_buttons.h"
 
 
+#define BASE_ADDRESS   (volatile uint8_t*)0x64000400
+#define EEPROM_SIZE   0x100
 
 /// END: To be programmed
 
 int main(void)
 {
     /// STUDENTS: To be programmed
+		uint8_t value;
+		while (1) {
+        for (uint16_t i = 0; i < EEPROM_SIZE; ++i) {
+            value = *(BASE_ADDRESS + i);      
+            if (value != (uint8_t)i) {
+                
+                CT_LED->BYTE.LED23_16 = i;
+                CT_LED->BYTE.LED7_0  = value;
 
-
-
+                while (!hal_ct_button_is_pressed(HAL_CT_BUTTON_T0));
+            }
+        }
+    }
 
     /// END: To be programmed
 }
